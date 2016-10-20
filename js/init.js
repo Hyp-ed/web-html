d58c3272f4c95e18efd01bcbbef6b469269e6175
felixxwu <felixxwu@gmail.com>
2016-10-20 10:11:07 +0100

    Added shuttle section

--- js/init.js
+++ js/init.js
@@ -1,7 +1,17 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+    	closeOnClick: true
+    });
+    
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+    $('.carousel.carousel-slider').carousel({full_width: true});
+
+    $('.carousel').carousel('next');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
