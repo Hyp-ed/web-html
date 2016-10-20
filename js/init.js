6d469e99fce80282f36abc65212fdaae7b261c1a
felixxwu <felixxwu@gmail.com>
2016-10-20 17:13:51 +0100

    smooth scrolling & better navigation

--- js/init.js
+++ js/init.js
@@ -4,19 +4,52 @@
     $('.button-collapse').sideNav({
     	closeOnClick: true
     });
+
+    $(document).scroll(function() {
+      var y = $(this).scrollTop();
+      if (y > 30) {
+        $('.menu-button').fadeIn();
+      } else {
+        $('.menu-button').fadeOut();
+      }
+    });
     
     $(document).ready(function(){
       $('.carousel').carousel();
       $('.default-hide').hide();
+
+      $(".tohome").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#home").offset().top
+        }, 1000);
+      });
+      $(".toabout").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#about").offset().top
+        }, 1000);
+      });
+      $(".toabout2").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#about2").offset().top
+        }, 1000);
+      });
+      $(".toteam").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#team").offset().top
+        }, 1000);
+      });
     });
 
     jQuery(document).ready(function(){
-        jQuery('#hideshowstatic').on('click', function(event) {        
-             jQuery('#contentstatic').toggle('hidden')('show');
-        });
-        jQuery('#hideshowsmart').on('click', function(event) {        
-             jQuery('#contentsmart').toggle('hidden')('show');
-        });
+      jQuery('#hideshowstatic').on('click', function(event) {        
+       jQuery('#contentstatic').toggle('hidden')('show');
+     });
+      jQuery('#hideshowsmart').on('click', function(event) {        
+       jQuery('#contentsmart').toggle('hidden')('show');
+     });
+      jQuery('#hideshowdynamic').on('click', function(event) {        
+       jQuery('#contentdynamic').toggle('hidden')('show');
+     });
     });
 
     $('.carousel.carousel-slider').carousel({full_width: true});
